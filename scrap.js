b7f17de660f9b59a40ef444c88ec93455120f406
Bartelus <wilroed@gmail.com>
2024-10-18 13:37:04 +0200

    lager array for level verdi

--- scrap.js
+++ scrap.js
@@ -1,9 +1,24 @@
+//definerer en beholder:
+
+let listeBeholder = document.querySelector("body");
+
+// Knytter knapp med 'click' funksjon som leser verdi og lager liste/m element:
+
 document.getElementById('levelSubmit').addEventListener('click', function(){
-const levelinput = document.getElementById('level');
-const levelValue = parseFloat(levelinput.value);
-
-levelList = document.createElement("li")
-levelOutput = document.createElement("h2")
-
+let levelinput = document.getElementById('level');
+let levelValue = parseFloat(levelinput.value);
 console.log(levelValue)
+
+let levelList = document.createElement("li");
+let levelOutput = document.createElement("h2");
+
+levelOutput.textContent = levelValue
+
+levelList.append(levelOutput);
+listeBeholder.append(levelList);
+listeBeholder.append(levelOutput);
+
+// Styling:
+levelList.className = "min-w-full h-12";
+levelOutput.className = "text-center text-yellow-500 text-3xl animate-pulse";
 });
